621ed56c4e2f8f5711cf9126eb306e18555ed88c
JSmith <65369149+JHussle@users.noreply.github.com>
2020-11-08 19:49:44 -0600

    init commits

---
+++ WiredBrainCoffee/Scenes/GiftViewController/TestCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TestCollectionViewCell.swift
+//  WiredBrainCoffee
+//
+//  Created by Joey Smith on 11/8/20.
+//  Copyright © 2020 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    
+}
